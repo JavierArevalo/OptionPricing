caf11a8c2157c3cd1690f46961ebfe3902eff8dc
Anson Tsang <ytsang3@gatech.edu>
2022-04-19 13:31:06 -0400

    it works now

--- scrape.py
+++ scrape.py
@@ -3,19 +3,20 @@ from yahoo_fin import stock_info as si
 
 
 class Option:
-    def _init_(self, ticker):
+    def __init__(self, ticker):
 
         Option = options.get_options_chain("nflx")
+        Option =  Option["calls"]
         dates = options.get_expiration_dates(ticker)
 
         self.ticker = ticker
         self.S = si.get_live_price(ticker)
-        self.K = Option.iloc[0,"Strike"]
-        self.R = Option.iloc[0,"Open Interest"]
-        self.V = Option.iloc[0,"Implied Volatility"]
-        self.B = Option.iloc[0,"Bid"]
-        self.A = Option.iloc[0,"Ask"]
-        self.T = dates(0)
+        self.K = Option.loc[0,"Strike"]
+        self.R = Option.loc[0,"Open Interest"]
+        self.V = Option.loc[0,"Implied Volatility"]
+        self.B = Option.loc[0,"Bid"]
+        self.A = Option.loc[0,"Ask"]
+        self.T = dates[0]
     
     def getOptionsData(self, ticker=None):
         if (ticker is None):
@@ -25,7 +26,7 @@ class Option:
 
         return optionData
 
-
+test = Option("AAPL")
 
 
 
