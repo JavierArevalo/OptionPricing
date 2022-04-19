3c45dad4df240e50adf94119e4beeef5a0dc0bb0
Anson Tsang <ytsang3@gatech.edu>
2022-04-19 13:20:09 -0400

    so stupid

--- scrape.py
+++ scrape.py
@@ -9,13 +9,12 @@ class Option:
         dates = options.get_expiration_dates(ticker)
 
         self.ticker = ticker
-
         self.S = si.get_live_price(ticker)
-        self.K = Option.loc[0,"Strike"]
-        self.R = Option.loc[0,"Open Interest"]
-        self.V = Option.loc[0,"Implied Volatility"]
-        self.B = Option.loc[0,"Bid"]
-        self.A = Option.loc[0,"Ask"]
+        self.K = Option.iloc[0,"Strike"]
+        self.R = Option.iloc[0,"Open Interest"]
+        self.V = Option.iloc[0,"Implied Volatility"]
+        self.B = Option.iloc[0,"Bid"]
+        self.A = Option.iloc[0,"Ask"]
         self.T = dates(0)
     
     def getOptionsData(self, ticker=None):
